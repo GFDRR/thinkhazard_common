faa80615b99a12732e3e56e13001dc5697df090c
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2016-01-19 16:03:40 +0100

    associate_admindiv_hazardcategory not required anymore

--- thinkhazard_common/utils.py
+++
@@ -1,8 +0,0 @@
-from models import HazardCategoryAdministrativeDivisionAssociation
-
-
-def associate_admindiv_hazardcategory(admindiv, hazardcategory, source):
-    a = HazardCategoryAdministrativeDivisionAssociation(source=source)
-    a.hazardcategory = hazardcategory
-    a.administrativedivision = admindiv
-    admindiv.hazardcategories.append(a)
